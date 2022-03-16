a0b7fe0357aa473f9a6f91c23db8e616f1c87217
DhanteyUD <otseclinton60@gmail.com>
2022-03-16 03:14:51 +0100

    Updated

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 // Random quote machine
 
-const newQuoteButton = document.querySelector('#new-quote');
-newQuoteButton.addEventListener('click', getQuote);
-
-var colors = [
+const colors = [
   '#16a085',
   '#27ae60',
   '#2c3e50',
@@ -17,44 +14,64 @@ var colors = [
   '#77B1A9',
   '#73A857',
 ];
-const endpoint =
-  'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json';
 
-// Defining async function
-async function getQuote() {
-  let index = Math.floor(Math.random() * endpoint.length);
+let currentQuote = '';
+let currentAuthor = '';
 
-  // Same method
-  // document.getElementById("text").innerHTML = endpoint[index].quote;
-  // document.getElementById("author").innerHTML = endpoint[index].author;
+function getQuotes() {
+  return $.ajax({
+    headers: {
+      Accept: 'application/json',
+    },
+    url: 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json',
+    success: (jsonQuotes) => {
+      if (typeof jsonQuotes === 'string') {
+        quotesData = JSON.parse(jsonQuotes);
+        console.log('quotesData');
+        console.log(quotesData);
+      }
+    },
+  });
+}
 
-  const myQuote = endpoint[index].quote;
-  const myAuth = endpoint[index].author;
+function getRandomQuote() {
+  return quotesData.quotes[
+    Math.floor(Math.random() * quotesData.quotes.length)
+  ];
+}
 
-  const myText = document.getElementById('text');
-  const myAuthor = document.getElementById('author');
+function getQuote() {
+  let randomQuote = getRandomQuote();
 
-  myText.innerHTML = myQuote;
-  myAuthor.innerHTML = myAuth;
+  currentQuote = randomQuote.quote;
+  currentAuthor = randomQuote.author;
 
-  try {
-    // Storing response
-    const response = await fetch(endpoint);
-    if (!response.ok) {
-      throw error(response.statusText);
-    }
+  $('#tweet-quote').attr(
+    'href',
+    'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' +
+      encodeURIComponent('"' + currentQuote + '" ' + currentAuthor)
+  );
 
-    // Storing data in form of JSON
-    const data = await response.json();
-    displayQuote(data.quotes[index].quote);
-    displayAuthor(data.quotes[index].author);
-  } catch {
-    alert('Failed to fetch quote, please try again!');
-  }
+  $('#tumblr-quote').attr(
+    'href',
+    'https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=' +
+      encodeURIComponent(currentAuthor) +
+      '&content=' +
+      encodeURIComponent(currentQuote) +
+      '&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button'
+  );
 
-  // Changing style of quote machine
+  $('.quote-text').animate({ opacity: 0 }, 500, function () {
+    $(this).animate({ opacity: 1 }, 500);
+    $('#text').text(randomQuote.quote);
+  });
+
+  $('.quote-author').animate({ opacity: 0 }, 500, function () {
+    $(this).animate({ opacity: 1 }, 500);
+    $('#author').html(randomQuote.author);
+  });
+
   var color = Math.floor(Math.random() * colors.length);
-
   $('html body').animate(
     {
       backgroundColor: colors[color],
@@ -62,8 +79,7 @@ async function getQuote() {
     },
     1000
   );
-
-  $('#new-quote').animate(
+  $('.button').animate(
     {
       backgroundColor: colors[color],
     },
@@ -71,49 +87,124 @@ async function getQuote() {
   );
 }
 
-function displayQuote(quote) {
-  const quoteText = document.querySelector('#text');
-  quoteText.textContent = quote;
-}
+$(document).ready(function () {
+  getQuotes().then(() => {
+    getQuote();
+  });
 
-function displayAuthor(author) {
-  const quoteAuthor = document.querySelector('#author');
-  quoteAuthor.textContent = author;
-}
+  $('#new-quote').on('click', getQuote);
+});
 
-// $('#tweet-quote').attr(
-//   'href',
-//   'https://twitter.com/intent/tweet?text=' +
-//     encodeURIComponent('"' + displayQuote() + '" ' + displayAuthor())
-// );
+https://www.facebook.com/
 
-// $('#facebook-quote').attr(
-//   'href',
-//   'https://www.facebook.com/sharer/sharer.php?=' +
-//     encodeURIComponent('"' + displayQuote() + '" ' + displayAuthor())
-// );
+// const newQuoteButton = document.querySelector('#new-quote');
+// newQuoteButton.addEventListener('click', getQuote);
 
-// $('#tumblr-quote').attr(
-//   'href',
-//   'https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=' +
-//     encodeURIComponent(displayAuthor()) +
-//     '&content=' +
-//     encodeURIComponent(displayQuote()) +
-//     '&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button'
-// );
+// var colors = [
+//   '#16a085',
+//   '#27ae60',
+//   '#2c3e50',
+//   '#f39c12',
+//   '#e74c3c',
+//   '#9b59b6',
+//   '#FB6964',
+//   '#342224',
+//   '#472E32',
+//   '#BDBB99',
+//   '#77B1A9',
+//   '#73A857',
+// ];
 
-// Calling that async function
-getQuote(endpoint);
+// let currentQuote = '';
+// let currentAuthor = '';
 
-// /* eslint-disable max-len */
-// // eslint-disable-next-line no-unused-vars
-// const projectName = 'random-quote-machine';
-// let quotesData;
+// const endpoint =
+//   'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json';
 
-// /*
-//   Code by Gabriel Nunes
-//   Modified by Todd Chaffee to use Camper gist for JSON Quote data.
-// */
+// // Defining async function
+// async function getQuote() {
+//   let index = Math.floor(Math.random() * endpoint.length);
+
+//   // Same method
+//   // document.getElementById("text").innerHTML = endpoint[index].quote;
+//   // document.getElementById("author").innerHTML = endpoint[index].author;
+
+//   const myQuote = endpoint[index].quote;
+//   const myAuth = endpoint[index].author;
+
+//   const myText = document.getElementById('text');
+//   const myAuthor = document.getElementById('author');
+
+//   myText.innerHTML = myQuote;
+//   myAuthor.innerHTML = myAuth;
+
+//   try {
+//     // Storing response
+//     const response = await fetch(endpoint);
+//     if (!response.ok) {
+//       throw error(response.statusText);
+//     }
+
+//     // Storing data in form of JSON
+//     const data = await response.json();
+//     displayQuote(data.quotes[index].quote);
+//     displayAuthor(data.quotes[index].author);
+//   } catch {
+//     alert('Failed to fetch quote, please try again!');
+//   }
+
+//   // Changing style of quote machine
+//   var color = Math.floor(Math.random() * colors.length);
+
+//   $('html body').animate(
+//     {
+//       backgroundColor: colors[color],
+//       color: colors[color],
+//     },
+//     1000
+//   );
+
+//   $('#new-quote').animate(
+//     {
+//       backgroundColor: colors[color],
+//     },
+//     1000
+//   );
+// }
+
+// function displayQuote(quote) {
+//   const quoteText = document.querySelector('#text');
+//   quoteText.textContent = quote;
+// }
+
+// function displayAuthor(author) {
+//   const quoteAuthor = document.querySelector('#author');
+//   quoteAuthor.textContent = author;
+// }
+
+// // $('#tweet-quote').attr(
+// //   'href',
+// //   'https://twitter.com/intent/tweet?text=' +
+// //     encodeURIComponent('"' + displayQuote() + '" ' + displayAuthor())
+// // );
+
+// // $('#facebook-quote').attr(
+// //   'href',
+// //   'https://www.facebook.com/sharer/sharer.php?=' +
+// //     encodeURIComponent('"' + displayQuote() + '" ' + displayAuthor())
+// // );
+
+// // $('#tumblr-quote').attr(
+// //   'href',
+// //   'https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=' +
+// //     encodeURIComponent(displayAuthor()) +
+// //     '&content=' +
+// //     encodeURIComponent(displayQuote()) +
+// //     '&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button'
+// // );
+
+// // Calling that async function
+// getQuote(endpoint);
 
 // var colors = [
 //   '#16a085',
